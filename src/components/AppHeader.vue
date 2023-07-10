ff9ad48c3973f1b2d55551e1f6efbf45582f18cc
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-07-10 16:19:12 +0200

    Added Jumbotron

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,24 +1,35 @@
 <script>
 import AppLogo from "./AppLogo.vue"
 import AppNavbar from "./AppNavbar.vue"
+import MainJumbotron from "./header-components/MainJumbotron.vue"
 export default {
-    components: { AppLogo, AppNavbar },
+    components: { AppLogo, AppNavbar, MainJumbotron },
     props: { navbarLists: Object }
 }
 </script>
 
 <template>
     <header>
-        <AppLogo />
-        <AppNavbar :navbarLists="navbarLists" />
+        <section>
+            <AppLogo />
+            <AppNavbar :navbarLists="navbarLists" />
+        </section>
+        <MainJumbotron />
     </header>
 </template>
 
 <style scoped lang="scss">
-header {
+section {
     display: flex;
     justify-content: space-between;
     align-items: center;
-    padding: 10px 30px;
+    padding: 10px 20px;
+}
+
+header {
+    background-image: url("../assets/images/hero-header-bakery.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: top;
 }
 </style>
---
+++ src/components/header-components/MainJumbotron.vue
@@ -0,0 +1,47 @@
+<script></script>
+<template>
+    <section id="jumbotron">
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <div>FRESH & TASTY BAKERY EVERY DAY</div>
+                    <h1>The Perfect Fresh Bread</h1>
+                    <p>Cras Consequat lectus vestibulm tortor pulvinar, quis euismod nisl varius. Aliquam erat volutpat.
+                        Nullam quis
+                        sagittis nibh ac nulla id orci tempor.
+                    </p>
+                    <button type="button" class="btn btn-light">Explore our products</button>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<style scoped lang="scss">
+@use "../../assets/scss/vars" as *;
+
+.col-4 {
+    div {
+        font-size: 0.8rem;
+        color: grey;
+    }
+
+    p {
+        color: grey;
+    }
+}
+
+h1 {
+    color: $primary;
+    font-weight: 800;
+}
+
+section {
+    height: 600px;
+}
+
+button {
+    font-size: 0.8rem;
+    color: $primary;
+    padding: 10px 20px;
+}
+</style>
