f2b6a611126e6188f24109a05a315ce532b72aa7
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-07-11 13:03:39 +0200

    Added SectionSharing

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,13 +4,15 @@ import SectionWedding from "./main-components/SectionWeddings.vue"
 import SectionAllProducts from "./main-components/SectionAllProducts.vue"
 import SectionIngredients from "./main-components/SectionIngredients.vue"
 import SectionLocation from "./main-components/SectionLocation.vue"
+import SectionSharing from "./main-components/SectionSharing.vue"
+
 
 
 
 
 export default {
     components: {
-        SectionProducts, SectionWedding, SectionAllProducts, SectionIngredients, SectionLocation
+        SectionProducts, SectionWedding, SectionAllProducts, SectionIngredients, SectionLocation, SectionSharing
     }
 }
 </script>
@@ -21,6 +23,8 @@ export default {
         <SectionAllProducts />
         <SectionIngredients />
         <SectionLocation />
+        <SectionSharing />
+
     </div>
 </template>
 <style scoped lang="scss"></style>
---
+++ src/components/main-components/SectionSharing.vue
@@ -0,0 +1,36 @@
+<script></script>
+
+<template>
+    <section id="sharing" class="mt-100">
+        <div class="row">
+            <div class="col-3 p-0">
+                <img src="../../assets/images/social-1.jpg" alt="SOCIAL" class="img-fluid">
+            </div>
+            <div class="col-6 p-0"></div>
+            <div class="col-3 p-0"><img src="../../assets/images/social-2.jpg" alt="SOCIAL" class="img-fluid"></div>
+            <div class="col-3 p-0">
+                <img src="../../assets/images/social-3.jpg" alt="SOCIAL" class="img-fluid">
+            </div>
+            <div class="col-3 p-0">
+                <img src="../../assets/images/social-4.jpg" alt="SOCIAL" class="img-fluid">
+            </div>
+            <div class="col-3 p-0">
+                <img src="../../assets/images/social-5.jpg" alt="SOCIAL" class="img-fluid">
+            </div>
+            <div class="col-3 p-0">
+                <img src="../../assets/images/social-6.jpg" alt="SOCIAL" class="img-fluid">
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped lang="scss">
+.col-6 {
+    background-image: linear-gradient(324deg, #f0edf5, 60%, #55328B 90%);
+    border: 10px solid white;
+}
+
+.col-3 {
+    border: 10px solid white;
+}
+</style>
