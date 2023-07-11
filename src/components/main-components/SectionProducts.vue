f82ae27b70f989947a3f6449ff4595cb1c17b7ec
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-07-11 17:56:57 +0200

    Added Hover Card Products

--- src/components/main-components/SectionProducts.vue
+++ src/components/main-components/SectionProducts.vue
@@ -23,12 +23,23 @@ export default {
                 <div class="my-card">
                     <img src="../../assets/images/choco-chip-cookies-400x510.jpg" alt="cookies" class="img-fluid">
                     <div class="angle-left angle">&langle;</div>
+                    <div class="on-hover">
+                        <h3>Choco Chip Cookies</h3>
+                        <div class="type">Cookies, Pastries</div>
+                        <div class="price">$19.00 - $39.00</div>
+                    </div>
                 </div>
+
             </CardProduction>
             <CardProduction>
                 <div class="my-card">
                     <img src="../../assets/images/strawberry-jam-cookies-400x510.jpg" alt="cookies" class="img-fluid">
                     <div class="angle-right angle">&rangle;</div>
+                    <div class="on-hover">
+                        <h3>Strawberry Jam Cookies</h3>
+                        <div class="type">Cookies, Pastries</div>
+                        <div class="price">$24.00 - $62.00</div>
+                    </div>
                 </div>
             </CardProduction>
         </div>
@@ -39,6 +50,10 @@ export default {
 
 .my-card {
     position: relative;
+
+    &:hover .on-hover {
+        display: block;
+    }
 }
 
 img {
@@ -47,5 +62,27 @@ img {
 
 p {
     color: grey;
+}
+
+.on-hover {
+    position: absolute;
+    top: 50%;
+    right: 50%;
+    transform: translate(50%, -50%);
+    color: white;
+    text-align: center;
+    display: none;
+
+    h3 {
+        font-size: 18px;
+    }
+
+    .type {
+        font-size: 10px;
+    }
+
+    .price {
+        font-size: 20px;
+    }
 }
 </style>
